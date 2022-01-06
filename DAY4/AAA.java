0e2979ee1f1c1bfd37b28b399dadba6b85926eab
summer0573 <83990983+summer0573@users.noreply.github.com>
2022-01-06 11:48:44 +0900

    Add files via upload

---
+++ DAY4/AAA.java
@@ -0,0 +1,11 @@
+package Test.t1;
+
+public class AAA {
+	public void accessTest() {
+		MainTest mt = new MainTest();
+		System.out.println(mt.n1);
+		//System.out.println(mt.n2);
+		System.out.println(mt.n3);
+		System.out.println(mt.n4);
+	}
+}
---
+++ DAY4/MainTest.java
@@ -0,0 +1,8 @@
+package Test.t1;
+
+public class MainTest {
+	public int n1 = 10;
+	private int n2 = 20;
+	protected int n3 = 30;
+	int n4 = 40;
+}
